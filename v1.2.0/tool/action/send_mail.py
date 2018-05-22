9d086ca29cdb3ac86d7ca2a9d30a652e9dc090c3
Liu Shuai <957419821@qq.com>
2018-05-22 13:23:14 +0800

    Update send_mail.py

--- v1.2.0/tool/action/send_mail.py
+++ v1.2.0/tool/action/send_mail.py
@@ -12,15 +12,18 @@ class SendMail():
         self.attachment = self.getCsvFile()
         # 准备发送的信息...
         self.smtp_message = self.createSmtpMessage()
-        
+
     def sendMail(self):
+        """
+            发送邮件
+        """
         try:
-            smtpObj = smtplib.SMTP() 
+            smtpObj = smtplib.SMTP()
             print('connecting smtp server...')
             smtpObj.connect(self.cd.smtp_host, self.cd.smtp_port)
             print('login smtp server...')
-            # 防止出现 smtplib.SMTPAuthenticationError: (530, b'Must issue a STARTTLS command first.') 异常
-            smtpObj.starttls() 
+            # 下一行防止出现 smtplib.SMTPAuthenticationError: (530, b'Must issue a STARTTLS command first.') 异常
+            smtpObj.starttls()
             smtpObj.login(self.cd.smtp_user, self.cd.smtp_pswd)
             print('sending...')
             smtpObj.sendmail(self.cd.smtp_send, self.cd.smtp_recv, self.smtp_message.as_string())
@@ -30,10 +33,16 @@ class SendMail():
             print("sorry, an error happend.")
 
     def createCD(self):
+        """
+            使用Configure类中的createConfigureData获取配置信息
+        """
         kfg = conf.Configure()
         return kfg.createConfigureData()
 
     def createSmtpMessage(self):
+        """
+            创建用于发送的message对象，里面包含有csv格式的附件
+        """
         msg = MIMEMultipart()
         msg['From'] = Header(self.cd.smtp_fm, 'utf-8')
         msg['To'] =  Header(self.cd.smtp_to, 'utf-8')
@@ -53,6 +62,9 @@ class SendMail():
         return msg
 
     def getCsvFile(self):
+        """
+            在main.py所在的目录下，寻找所有csv文件，并以列表的形式返回
+        """
         return [d for d in os.listdir() if d[-4:]=='.csv']
 
 if __name__ == "__main__":
