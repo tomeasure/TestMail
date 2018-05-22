5dfbefadecaee740fd72d6f33727ecc0b71af974
Liu Shuai <957419821@qq.com>
2018-05-22 13:21:49 +0800

    Update custom_email.py

--- v1.2.0/tool/custom_email.py
+++ v1.2.0/tool/custom_email.py
@@ -1,12 +1,18 @@
 from tool.action import send_mail, recv_mail
 
-class Email():  
+class Email():
+    def __init__(self):
+        pass
     def sendMail(self):
+        # 实例化SendMail类
         sm = send_mail.SendMail()
+        # 发送邮件
         sm.sendMail()
 
     def recvMail(self):
+        # 实例化RecvMail类
         rm = recv_mail.RecvMail()
+        # 接收邮件
         rm.recvMail()
 
 if __name__ == "__main__":
